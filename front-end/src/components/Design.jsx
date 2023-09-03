c96dfd14268f9aee1b04de0a8925ae2c5faf6291
abitshrestha47 <abitshrestha2079@gmail.com>
2023-09-03 09:18:01 +0545

    changes

--- front-end/src/components/Design.jsx
+++ front-end/src/components/Design.jsx
@@ -1,4 +1,4 @@
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Design = () => {
@@ -6,53 +6,65 @@ const Design = () => {
     var quoteParsed;
 
     //setting localstorage assign the value with variable quote&date
-    if(localStorage.length>0){
-        const initialQuote=localStorage.getItem('quote&date');
-        quoteParsed=JSON.parse(initialQuote);
-    }else{
-        quoteParsed='';
+    if (localStorage.length > 0) {
+        const initialQuote = localStorage.getItem('quote&date');
+        quoteParsed = JSON.parse(initialQuote);
+    } else {
+        quoteParsed = '';
     }
 
     //setting the quote if empty '' else from localstoagrage
-    const [quote,setQuote]=useState(quoteParsed);
+    const [quote, setQuote] = useState(quoteParsed);
 
     //get quote i.e new
-    const getQuote=async ()=>{
-        try{
-            const response=await axios.get(`http://localhost:2000/getQuote`)
+    const getQuote = async () => {
+        try {
+            const response = await axios.get(`http://localhost:2000/getQuote`)
             console.log(response);
             setQuote(response.data);
-            const todayDate=new Date().toISOString();
-            const newQuote=JSON.parse(response.data);
-            console.log(typeof(newQuote[0]));
+            const todayDate = new Date().toISOString();
+            const newQuote = JSON.parse(response.data);
+            console.log(typeof (newQuote[0]));
             console.log(newQuote);
-            newQuote[0].todayDate0=todayDate;
+            newQuote[0].todayDate0 = todayDate;
             console.log(newQuote);
             const newQuoteString = JSON.stringify(newQuote);
-            localStorage.setItem('quote&date',newQuoteString);
-        }catch(err){    
+            localStorage.setItem('quote&date', newQuoteString);
+        } catch (err) {
             console.log(`Error:${err}`);
         }
     }
-
-    //if empty get the new quote(first inital mount check if empty then executes)
-    useEffect(()=>{
-            //if quote is not present 
-            if(!quote){
+    const checkDate = () => {
+        if (quote && quote[0] && quote[0].todayDate0) {
+            const checkDate = new Date().toISOString().split('T')[0];
+            const getLocal = quote[0].todayDate0.split('T')[0];
+            if (getLocal !== checkDate) {
+                console.log('fdf');
+                localStorage.clear();
                 getQuote();
             }
-    },[]);
+        }
+
+    }
+    //if empty get the new quote(first inital mount check if empty then executes)
+    useEffect(() => {
+        //if quote is not present 
+        if (!quote) {
+            getQuote();
+        }
+        checkDate();
+    }, [quote]);
 
     return (
         <div className="body">
             <div className="container">
                 <h1>
                     {/* quote is  represented by array of objects has only one object inside array so to get first array which is obj & getting its quote value*/}
-                    <span>{quote?(quote[0].quote):''}</span>
+                    <span>{quote ? (quote[0].quote) : ''}</span>
                 </h1>
-                <br/>
+                <br />
                 {/* similar but for author */}
-                <h4>&mdash;{quote?quote[0].author:''}</h4>
+                <h4>&mdash;{quote ? quote[0].author : ''}</h4>
             </div>
         </div>
     )
